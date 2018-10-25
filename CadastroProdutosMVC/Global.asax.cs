67cf4889e266c2f58753b59f1fdccca130c335cb
Lucas Beck <lucas.beck@cwi.com.br>
2018-10-25 09:50:12 -0300

    Teste

--- CadastroProdutosMVC/Global.asax.cs
+++ CadastroProdutosMVC/Global.asax.cs
@@ -16,6 +16,8 @@ namespace CadastroProdutosMVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            
         }
     }
 }
